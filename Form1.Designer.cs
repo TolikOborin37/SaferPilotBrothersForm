a6d56ae219ace2de53eabef6b3ed627c6b8afc25
TolikOborin37 <tolik.oborin.1996@gmail.com>
2022-09-26 19:26:22 +0400

    Сейф братьев пилотов

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace SaferPilotBrothersForm
+{
+    partial class FormGame
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormGame));
+            this.GameName = new System.Windows.Forms.Label();
+            this.rules = new System.Windows.Forms.Label();
+            this.numSize = new System.Windows.Forms.NumericUpDown();
+            this.butStartGame = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GameName
+            // 
+            this.GameName.AutoSize = true;
+            this.GameName.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.GameName.Location = new System.Drawing.Point(57, 86);
+            this.GameName.Name = "GameName";
+            this.GameName.Size = new System.Drawing.Size(566, 110);
+            this.GameName.TabIndex = 0;
+            this.GameName.Text = "Логическая игра\r\n\"Сейф братьев пилотов\"\r\n";
+            this.GameName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // rules
+            // 
+            this.rules.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rules.Location = new System.Drawing.Point(12, 209);
+            this.rules.Name = "rules";
+            this.rules.Size = new System.Drawing.Size(611, 357);
+            this.rules.TabIndex = 1;
+            this.rules.Text = resources.GetString("rules.Text");
+            // 
+            // numSize
+            // 
+            this.numSize.Location = new System.Drawing.Point(12, 466);
+            this.numSize.Name = "numSize";
+            this.numSize.Size = new System.Drawing.Size(120, 23);
+            this.numSize.TabIndex = 2;
+            this.numSize.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numSize.ValueChanged += new System.EventHandler(this.numSize_ValueChanged);
+            // 
+            // butStartGame
+            // 
+            this.butStartGame.Location = new System.Drawing.Point(240, 590);
+            this.butStartGame.Name = "butStartGame";
+            this.butStartGame.Size = new System.Drawing.Size(132, 23);
+            this.butStartGame.TabIndex = 3;
+            this.butStartGame.Text = "Начать игру";
+            this.butStartGame.UseVisualStyleBackColor = true;
+            this.butStartGame.Click += new System.EventHandler(this.butStartGame_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SaferPilotBrothersForm.Properties.Resources._16906_original;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(213, 133);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormGame
+            // 
+            this.ClientSize = new System.Drawing.Size(635, 625);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.butStartGame);
+            this.Controls.Add(this.numSize);
+            this.Controls.Add(this.rules);
+            this.Controls.Add(this.GameName);
+            this.Name = "FormGame";
+            this.Text = "Сейф братьев пилотов";
+            ((System.ComponentModel.ISupportInitialize)(this.numSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Label GameName;
+        private System.Windows.Forms.Label rules;
+        private System.Windows.Forms.NumericUpDown numSize;
+        private System.Windows.Forms.Button butStartGame;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaferPilotBrothersForm
+{
+
+    public partial class FormGame : Form
+    {
+        //сообщение, которое выводится, когда игра пройдена
+        string message = "Сейф открылся.\n" +
+                    "Перезапустить игру?";
+        string titleMsg = "Сделай выбор";
+        MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+
+        //рандом, для присваивания изображений к кнопкам в случайном порядке
+        Random random = new Random();
+
+        //объект таблицы на форме
+        TableLayoutPanel playing_field = new TableLayoutPanel();
+
+        //лист изображений
+        List<Image> test = new List<Image>()
+        {
+            Properties.Resources.Gorizontal,
+            Properties.Resources.Vertical
+        };
+
+        //звуки игры
+        SoundPlayer klick = new SoundPlayer(Properties.Resources.buttonKlick1);
+        SoundPlayer open = new SoundPlayer(Properties.Resources.open1);
+
+        public FormGame()
+        {
+            InitializeComponent();
+
+        }
+
+        //метод для игры
+        private void Game(int size)
+        {
+
+            //расположение таблицы, относительно формы (левый верхний угол)
+            playing_field.Location = new Point(0, 0);
+            playing_field.AutoSize = false;
+            playing_field.CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset;
+            playing_field.Dock = DockStyle.Fill;
+
+            //процентное соотношение кнопок(чтобы все кнопки были равные по размеру между собой)
+            float percent = 100 / size;
+
+            //цикл добавления кнопок в таблицу
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Button button = new Button();
+                    button.Dock = DockStyle.Fill;
+                    button.BackColor = Color.Turquoise;
+                    button.Click += btn_click;
+                    playing_field.Controls.Add(button, i, j);
+                    playing_field.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, percent));
+                    playing_field.RowStyles.Add(new RowStyle(SizeType.Percent, percent));
+                    button.BackgroundImage = test[random.Next(0, 2)];
+                    button.BackgroundImageLayout = ImageLayout.Stretch;
+                }
+            }
+
+            Controls.Add(playing_field);
+        }
+
+        //событие по нажатию кнопки
+        private void btn_click(object sender, EventArgs e)
+        {
+
+            klick.Play();
+            //счетчики, необхожимый для определения, что игра пройдена
+            int countTrue = 0;
+            int countFalse = 0;
+
+            //меняем кнопку
+            if (((Button)sender).BackgroundImage == test[0])
+            {
+                ((Button)sender).BackgroundImage = test[1];
+            }
+            else
+            {
+                ((Button)sender).BackgroundImage = test[0];
+            }
+
+            //цикл подмены кнопок по гормзонтали и вертикали 
+            foreach (Control control in playing_field.Controls.OfType<Button>())
+            {
+                if (playing_field.GetRow(control) == playing_field.GetPositionFromControl((Button)sender).Row)
+                {
+                    if (control.BackgroundImage == test[0])
+                        control.BackgroundImage = test[1];
+                    else
+                        control.BackgroundImage = test[0];
+                }
+                if (playing_field.GetColumn(control) == playing_field.GetPositionFromControl((Button)sender).Column)
+                {
+                    if (control.BackgroundImage == test[0])
+                        control.BackgroundImage = test[1];
+                    else
+                        control.BackgroundImage = test[0];
+                }
+            }
+
+
+            //проверка всех кнопок
+            foreach (Control control in playing_field.Controls.OfType<Button>())
+            {
+                if (control.BackgroundImage == test[0])
+                    countTrue++;
+                else if (control.BackgroundImage == test[1])
+                    countFalse++;
+
+            }
+            if (countFalse == (playing_field.ColumnCount * playing_field.RowCount))
+            {
+                open.Play();
+
+                DialogResult result = MessageBox.Show(message, titleMsg, buttons);
+                if (result == DialogResult.Yes)
+                {
+                    Application.Restart();
+
+                }
+                else
+                {
+                    this.Close();
+                }
+
+            }
+            if (countTrue == (playing_field.ColumnCount * playing_field.RowCount))
+            {
+                open.Play();
+
+                DialogResult result = MessageBox.Show(message, titleMsg, buttons);
+                if (result == DialogResult.Yes)
+                {
+                    Application.Restart();
+
+                }
+                else
+                {
+                    this.Close();
+                }
+
+            }
+            countFalse = 0;
+            countTrue = 0;
+
+        }
+
+        private void numSize_ValueChanged(object sender, EventArgs e)
+        {
+
+            if (numSize.Value <= 1)
+            {
+                MessageBox.Show("Нельзя вводить число меньше или равное 0");
+                numSize.Value = 2;
+            }
+            if (numSize.Value % 2 != 0)
+            {
+                numSize.Value++;
+            }
+        }
+
+        private void butStartGame_Click(object sender, EventArgs e)
+        {
+            GameName.Visible = false;
+            rules.Visible = false;
+            numSize.Visible = false;
+            butStartGame.Visible = false;
+            pictureBox1.Visible = false;
+            Game((int)numSize.Value);
+        }
+    }
+}
+
